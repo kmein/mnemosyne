5b996f8269040c01228c50946a22db7cd0181bcb
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2018-09-19 12:25:18 +0200

    Nixify

---
+++ shell.nix
@@ -0,0 +1,36 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, bytestring, cassava, HaTeX, megaparsec
+      , optparse-applicative, prettyprinter, stdenv, text
+      }:
+      mkDerivation {
+        pname = "quote-db";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = true;
+        isExecutable = true;
+        libraryHaskellDepends = [
+          base bytestring cassava HaTeX megaparsec prettyprinter text
+        ];
+        executableHaskellDepends = [ base optparse-applicative text ];
+        testHaskellDepends = [ base ];
+        homepage = "https://github.com/kmein/quote-db#readme";
+        description = "A tool for managing a database of literature quotes";
+        license = stdenv.lib.licenses.mit;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
