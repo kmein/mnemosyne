32506f064ad854c5a67226ac605feac99fbfabbf
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2018-09-20 13:17:25 +0200

    Allow more specific search

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,7 @@ module Main
 
 import QuoteDb
 
-import Data.List (nub, sort)
+import Data.List (nub, sort, intersect)
 import Data.Monoid ((<>))
 import qualified Data.Text as Text
   ( Text
@@ -24,56 +24,56 @@ data QuoteOutputType
   | LaTeX
   deriving (Show)
 
-data QuoteComponent
-  = Quotes
-  | Sources
-  | Authors
+data Search
+  = SearchQuotes { pattern :: Text.Text }
+  | SearchSources { pattern :: Text.Text }
+  | SearchAuthors { pattern :: Text.Text }
   deriving (Show, Eq)
 
 data QuoteDbOptions = QuoteDbOptions
   { _dbFile :: FilePath
   , _outputType :: QuoteOutputType
   , _outputFont :: Maybe Font
-  , _searchDomain :: Maybe [QuoteComponent]
-  , _searchPattern :: Maybe Text.Text
+  , _searches :: Maybe [Search]
   } deriving (Show)
 
 quoteDbOptions :: Parser QuoteDbOptions
 quoteDbOptions =
   QuoteDbOptions <$> quoteDbFile <*> quoteOutputType <*> quoteOutputFont <*>
-  quoteSearchDomain <*>
-  quoteSearchPattern
+  quoteSearchDomain
   where
-    quoteDbFile =
-      strOption
-        (long "db-file" <> short 'f' <> metavar "CSV" <>
-         help "CSV quote database")
+    quoteDbFile = strArgument (metavar "CSV" <> help "CSV quote database")
     quoteOutputType =
       flag' Plain (long "plain" <> help "Output in plain text (default)") <|>
       flag Plain LaTeX (long "latex" <> help "Output in LaTeX")
     quoteSearchDomain =
       optional . fmap nub . some $
-      flag' Quotes (short 'q' <> long "quotes" <> help "Search quotes") <|>
-      flag' Sources (short 's' <> long "sources" <> help "Search sources") <|>
-      flag' Authors (short 'a' <> long "authors" <> help "Search authors")
-    quoteSearchPattern =
-      optional $
-      Text.pack <$> strArgument (metavar "PATTERN" <> help "Search string")
+      fmap
+        (SearchQuotes . Text.pack)
+        (strOption
+           (short 'q' <> long "quotes" <> help "Search quotes" <> metavar "STR")) <|>
+      fmap
+        (SearchSources . Text.pack)
+        (strOption
+           (short 's' <> long "sources" <> help "Search sources" <>
+            metavar "STR")) <|>
+      fmap
+        (SearchAuthors . Text.pack)
+        (strOption
+           (short 'a' <> long "authors" <> help "Search authors" <>
+            metavar "STR"))
     quoteOutputFont =
       optional $
       Text.pack <$>
       strOption
-        (long "font" <> metavar "FONTNAME" <>
+        (short 'f' <> long "font" <> metavar "FONTNAME" <>
          help "The font to be used for LaTeX output")
 
 main :: IO ()
 main = do
-  QuoteDbOptions df ot fo sd sp <- execParser quoteDbOptions'
+  QuoteDbOptions df ot fo ss <- execParser quoteDbOptions'
   quotes <- fileToQuotes df
-  let matching =
-        case sp of
-          Nothing -> quotes
-          Just pat -> searchQuotes sd pat quotes
+  let matching = maybe quotes (`searchQuotes` quotes) ss
   Text.putStrLn $
     flip id (sort matching) $
     case ot of
@@ -82,12 +82,11 @@ main = do
   where
     wrap at = Text.replace at ('\n' `Text.cons` at)
     quoteDbOptions' = info (quoteDbOptions <**> helper) fullDesc
-    searchQuotes dom pat =
-      filter $ \q ->
-        any (Text.isInfixOf pat) $ concatMap (select q) $ sequence dom
-    select (Quote a s _ q) =
-      maybe ([a, s] ++ q) $ \dom ->
-        case dom of
-          Quotes -> q
-          Sources -> [s]
-          Authors -> [a]
+    searchQuotes ss qs =
+      foldl1 intersect $
+      map (\s -> filter (any (Text.isInfixOf (pattern s)) . select s) qs) ss
+    select search (Quote a s _ q) =
+      case search of
+        SearchQuotes {} -> q
+        SearchSources {} -> [s]
+        SearchAuthors {} -> [a]
